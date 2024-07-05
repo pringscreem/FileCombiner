945b44578cf5874ca4f225b6d1f331dfc11374b2
Scott Sonnleitner <sonnleitner.s@gmail.com>
2024-07-04 23:10:13 -0600

    Update FileCombine.cpp

--- FileCombine.cpp
+++ FileCombine.cpp
@@ -8,6 +8,7 @@ int main(int argc, char* argv[])
     //std::ofstream outputFile;
     std::string outputFilename = static_cast<std::string>(argv[2]) + ".cpp"; //argv[2] should be the output name (second argument given)
     //std::cout << "The outputFilename is " << outputFilename << std::endl;
+    std::ofstream outputFile;
     outputFile.open(outputFilename, std::ios_base::app); //Open the string held in outputFilename ("argv[2].cpp") in append mode.
 
     //We need to get the argv and append the file extension to it (".cpp").
@@ -19,6 +20,17 @@ int main(int argc, char* argv[])
     for(int i = 2; i < argc; i++)
     {
          std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
+         std::ifstream inputFile;
+         inputFile.open(static_cast<std::string>(argv[i]));
+         std::string outputString;
+         int count = 0;
+         while(std::getline(inputFile, outputString))
+         {
+            outputFile << outputString;
+            std::cout << "Line " << count << " added to " << outputFilename << std::endl;
+            count++;
+         }
+        //  outputFile << inputFile; //Incorrect... We have to use getline instead (and go through a loop)
     }
 
     outputFile.close();
