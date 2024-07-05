152d5f6cda9a0ac2020fba188129cbe4a7801d43
Scott Sonnleitner <sonnleitner.s@gmail.com>
2024-07-04 22:40:17 -0600

    Update FileCombine.cpp

--- FileCombine.cpp
+++ FileCombine.cpp
@@ -9,11 +9,17 @@ int main(int argc, char* argv[])
     std::string outputFilename = static_cast<std::string>(argv[2]) + ".cpp"; //argv[2] should be the output name (second argument given)
     //std::cout << "The outputFilename is " << outputFilename << std::endl;
     outputFile.open(outputFilename, std::ios_base::app); //Open the string held in outputFilename ("argv[2].cpp") in append mode.
+
     //We need to get the argv and append the file extension to it (".cpp").
     //The argv might have to be cast to a string.
 
     //We do a for-loop that declares a new input filestream, initializes it to the current argv[i] string
     //as its' filename, and appends the contents of that file to the output file.
+
+    for(int i = 2; i < argc; i++)
+    {
+         std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
+    }
 
     outputFile.close();
     return 0;
