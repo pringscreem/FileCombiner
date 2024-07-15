baceca5d75fb2c4859a17eb8d1ed6b44b67671c3
scottsonnleitner <71006436+pringscreem@users.noreply.github.com>
2024-07-15 00:27:12 -0600

    Recompile

--- OutputFile.cpp
+++ OutputFile.cpp
@@ -32,7 +32,7 @@ void Game::RunGuessingGame()
 
     while(!isCorrectGuess)
     {
-        std::cout << "Enter your guess between" << myMin << " and " << myMax << ": ";
+        std::cout << "Enter your guess between " << myMin << " and " << myMax << ": ";
         std::cin >> guess;
         if(guess == randomNumber)
         {
