dac1c275bd1ba62a6d0f06d9c0798f3e5728386d
scottsonnleitner <71006436+pringscreem@users.noreply.github.com>
2024-07-28 00:29:33 -0600

    Fixed

--- Working Files/FunctionPointer.cpp
+++ Working Files/FunctionPointer.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 
 void myFunction();
+//void myFunction()
+//{
+//	std::cout << "myFunction\n";
+//}
+
+void callFuncPtr(void (*fPtr)());
+//void callFuncPtr(void (*fPtr)())
+//{
+//	fPtr();
+//}
 
 int main(int argc, char* argv[])
 {
 	void (*myFuncPtr)() = &myFunction;
 
 	std::cout << "Before calling myFuncPtr\n";
-	myFuncPtr;
+	myFuncPtr();
 	std::cout << "After calling myFuncPtr\n";
+
+	std::cout << "Before calling callFuncPtr\n";
+	callFuncPtr(myFuncPtr());
+	std::cout << "After calling callFuncPtr\n";
 
 	return 0;
 }
@@ -17,3 +31,39 @@ void myFunction()
 {
 	std::cout << "myFunction\n";
 }
+
+void callFuncPtr(void (*fPtr)())
+{
+	fPtr;
+}
+
+
+////Chat GPT Example
+//#include <iostream>
+//
+//// Define a function that we will call using a function pointer
+//void greet()
+//{
+//	std::cout << "Hello, World!" << std::endl;
+//}
+//
+//// Define another function that takes a function pointer as a parameter
+//void executeFunction(void (*funcPtr)())
+//{
+//// Call the function using the function pointer
+//	funcPtr();
+//}
+//
+//int main()
+//{
+//// Declare a function pointer and assign it to the greet function
+//	void (*funcPtr)() = &greet;
+//
+//	// Call the function directly using the function pointer
+//	funcPtr();
+//
+//	// Call the function using another function that takes a function pointer as an argument
+//	executeFunction(funcPtr);
+//
+//	return 0;
+//}
